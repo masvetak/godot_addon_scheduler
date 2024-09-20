91c8ae08720a569baa838fc82b3490a02d96b507
Marko Švetak <masvetak@gmail.com>
2024-09-20 09:56:45 +0200

    Implement timeout

--- scheduler.gd
+++ scheduler.gd
@@ -61,6 +61,40 @@ func cancel_ticker(id: int) -> void:
 	
 	mutex.unlock()
 
+func set_timeout(callable: Callable, timeout_msec: int, timeout_name: String = "") -> int:
+	mutex.lock()
+	
+	# increment id
+	_timeouts_id = _timeouts_id + 1
+	
+	# add timeout
+	var timeout: Dictionary = {
+		'id': _timeouts_id,
+		'callable': callable,
+		'timeout': timeout_msec,
+		'started_at': Time.get_ticks_msec(),
+		'name': timeout_name
+	}
+	_timeouts.append(timeout)
+	print("[SCHEDULER] added timeout id=%s with timeout %d msec" % [_timeouts_id, timeout_msec])
+	mutex.unlock()
+	
+	return _timeouts_id
+
+func cancel_timeout(id: int) -> void:
+	mutex.lock()
+	
+	var idx_to_remove: int = -1
+	for idx in range(_timeouts.size()):
+		if _timeouts[idx]['id'] == id:
+			idx_to_remove = idx
+			break
+	if idx_to_remove != -1:
+		_timeouts.remove_at(idx_to_remove)
+		print("[SCHEDULER] canceling timeout id=%s" % id)
+	
+	mutex.unlock()
+
 # ------------------------------------------------------------------------------
 # Private methods
 # ------------------------------------------------------------------------------
@@ -70,9 +104,11 @@ func _thread_loop() -> void:
 		if Time.get_ticks_msec() - _time_ticks_msec > min_ticks_msec_period:
 			_time_ticks_msec = Time.get_ticks_msec()
 			_handler()
+		await get_tree().create_timer(min_ticks_msec_period / 1000).timeout
 
 func _handler() -> void:
 	_handle_tickers()
+	_handle_timeouts()
 
 func _handle_tickers() -> void:
 	mutex.lock()
@@ -81,5 +117,20 @@ func _handle_tickers() -> void:
 		if Time.get_ticks_msec() - ticker['handled_at'] > ticker['period']:
 			ticker['callable'].call_deferred()
 			ticker['handled_at'] = Time.get_ticks_msec()
+	
+	mutex.unlock()
+
+func _handle_timeouts() -> void:
+	mutex.lock()
+	
+	var handled_timeout_ids_list: Array = []
+	for idx in _timeouts.size():
+		var timeout: Dictionary = _timeouts[idx]
+		if Time.get_ticks_msec() - timeout['started_at'] > timeout['timeout']:
+			timeout['callable'].call_deferred()
+			handled_timeout_ids_list.append(idx)
+	
+	for handled_timeout_idx in handled_timeout_ids_list:
+		_timeouts.remove_at(handled_timeout_idx)
 	
 	mutex.unlock()
